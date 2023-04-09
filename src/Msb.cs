698e8bec9601a1d474f938aa910e8441e83fbb7f
Swirek3331 <karolsutkowski21@gmail.com>
2023-04-09 17:20:22 +0100

    Kolejne okna dialogowe

--- src/Msb.cs
+++ src/Msb.cs
@@ -25,5 +25,32 @@ namespace Omlcs.src
         {
             MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        public static void Close()
+        {
+            DialogResult result = MessageBox.Show("Czy na pewno chcesz zamknąć program?", "Zamykanie", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        public static void Close(string message)
+        {
+            DialogResult result = MessageBox.Show(message, "Zamykanie", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        public static void Close(string title, string message)
+        {
+            DialogResult result = MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
     }
 }

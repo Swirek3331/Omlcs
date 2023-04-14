96fda76b87f95f2bfc08ff66cbec5969f0cd6781
Swirek3331 <karolsutkowski21@gmail.com>
2023-04-14 17:28:18 +0100

    Pudełka generacja

--- src/Summon.cs
+++ src/Summon.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Omlcs.src;
+using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 
 namespace Omlcs
 {
     public class Summon
     {
-        public static void Make()
+        public static void MakeBox(Control container)
         {
-            
+            PictureBox box = new PictureBox
+            {
+                Size = Randomite.RandomSize(10, 100),
+                Location = Randomite.RandomPosition(container),
+                BackColor = Randomite.RandomColor()
+            };
+
+            container.Controls.Add(box);
         }
     }
 }

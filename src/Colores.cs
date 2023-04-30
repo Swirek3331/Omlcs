743dc40cc3d81b94821a7350740f0bc6e426e3d2
Swirek3331 <karolsutkowski21@gmail.com>
2023-04-30 10:36:58 +0100

    Kolorek

--- src/Colores.cs
+++ src/Colores.cs
@@ -1,26 +1,21 @@
 ﻿using System;
 using System.Drawing;
-using System.Windows.Forms;
-using System.Globalization;
+
+using Omlcs.src.util;
 
 namespace Omlcs.src
 {
     public static class Colores
-    {
+    {   
         public static Color HexColor(string hex)
         {
             int offset = (hex[0] == '#') ? 1 : 0;
             
-            byte r = ParseHex(hex, 0 + offset, 2 + offset);
-            byte g = ParseHex(hex, 2 + offset, 4 + offset);
-            byte b = ParseHex(hex, 4 + offset, 6 + offset);
+            byte r = NumSys.ParseHex(hex, 0 + offset, 2 + offset);
+            byte g = NumSys.ParseHex(hex, 2 + offset, 4 + offset);
+            byte b = NumSys.ParseHex(hex, 4 + offset, 6 + offset);
 
             return Color.FromArgb(r, g, b);
-        }
-
-        private static byte ParseHex(string hex, int index, int lenght)
-        {
-            return byte.Parse(hex.Substring(index, lenght - index), NumberStyles.HexNumber);
         }
     }
 }

6a493a4a45481434d20a899e3a4a13fd2642e2cf
Swirek3331 <karolsutkowski21@gmail.com>
2023-04-30 10:59:08 +0100

    Sortowanie bąbelkowe

---
+++ src/util/NumSys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Omlcs.src.util
+{
+    public static class NumSys
+    {
+        public static byte ParseHex(string hex, int index, int lenght)
+        {
+            return byte.Parse(hex.Substring(index, lenght - index), NumberStyles.HexNumber);
+        }
+
+        //Będzie tu lepiej i wiecej, ale kiedyś...
+        /*
+         * > I tyle wamm musi na razie wystarczyć
+         * > "kiedyś"
+         */
+    }
+}
---
+++ src/util/Sorting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omlcs.src.util
+{
+    public static class Sorting
+    {
+        //Na razie go tak zrobiłem, ale może później będzie wydajniejszy
+        //Wciąż i tak O(n^2)
+        //Na chwilę obecną tylko int
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}

af3a7de94cab524e3319c6f3ae12639abad7f099
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-01 11:09:30 +0100

    Ikona

--- src/com/Forms.cs
+++ src/com/Forms.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Omlcs.src.com
 {
     public static class Forms
     {
+        public static Icon icon = new Icon("icon.ico");//Nie wiem czy to wyrkyję z /asstes/spirtes.
+
         public static void ShowForm(Form form, Form parrent)
         {
             form.Icon = parrent.Icon;

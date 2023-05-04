a562cc86fd05a5754a9487ebe25cdf6d15240ef4
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-04 15:14:33 +0100

    Poprawka

--- src/util/Search.cs
+++ src/util/Search.cs
@@ -6,17 +6,17 @@ namespace Omlcs.src.util
     public static class Search
     {
         //Nie mam pomysłu na to wszystko jszcze.
-        public static bool IsInArray<T>(T[] array, T value)
+        public static int LinearSearch<T>(T[] array, T value)
         {
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i].Equals(value))
                 {
-                    return true;
+                    return i;
                 }
                 break;
             }
-            return false;
+            return -1;
         }
     }
 }

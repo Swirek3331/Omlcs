cb3a124271680ff41c44dd3ff3fe27cef010b5cf
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-04 16:19:33 +0100

    Opis i przeciążenia

--- src/util/Search.cs
+++ src/util/Search.cs
@@ -6,9 +6,45 @@ namespace Omlcs.src.util
     public static class Search
     {
         //Nie mam pomysłu na to wszystko jszcze.
+
+        /// <summary>
+        /// Wyszukiwanie liniowe.
+        /// Przeszukuję każdy indeks tablicy.
+        /// Złożoność czasowa: O(n)
+        /// </summary>
+        /// <typeparam name="T">Trzyszcz</typeparam>
+        /// <param name="array">Tablica do przeszukania</param>
+        /// <param name="value">Wartość poszukiwana</param>
+        /// <returns>Najbliższy indeks zawierający wartość poszukiwaną. Jeśli nie ma jej nigdzie zwraca -1</returns>
         public static int LinearSearch<T>(T[] array, T value)
         {
             for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    return i;
+                }
+                break;
+            }
+            return -1;
+        }
+
+        public static int LinearSearch<T>(T[] array, T value, int start)
+        {
+            for (int i = start; i < array.Length; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    return i;
+                }
+                break;
+            }
+            return -1;
+        }
+
+        public static int LinearSearch<T>(T[] array, T value, int start, int end)
+        {
+            for (int i = start; i < end; i++)
             {
                 if (array[i].Equals(value))
                 {

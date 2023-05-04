19053370f848c3314e148e71638795cbcd5c9082
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-04 14:22:18 +0100

    Zmiana nazwy

--- src/util/Sort.cs
+++ src/util/Sort.cs
@@ -16,9 +16,9 @@ namespace Omlcs.src.util
                 {
                     if (array[j] > array[j+1])
                     {
-                        int temp = array[j];
-                        array[j] = array[j+1];
-                        array[j+1] = temp;
+                        int temp = array[j+1];
+                        array[j+1] = array[j];
+                        array[j] = temp;
                     }
                 }
             }

dc639238c11c52e1bb137c49210ad2eeb7f328d1
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-04 18:04:50 +0100

    Po swojemu

---
+++ src/forms/Output1.Designer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omlcs.src.forms
+{
+    //Zrobię to poswojemu, jebać microsoft.
+    partial class Output1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Output1";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ src/forms/Output1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Omlcs.src.forms
+{
+    public partial class Output1 : Form
+    {
+        public Output1()
+        {
+            InitializeComponent();
+        }
+    }
+}

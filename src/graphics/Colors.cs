a2ccbb80fc63f4e6c7b2ce62078ac82af5efab90
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-07 15:17:58 +0100

    HSL?

--- src/graphics/Colors.cs
+++ src/graphics/Colors.cs
@@ -13,7 +13,7 @@ namespace Omlcs.src.graphics
         /// </summary>
         /// <param name="hex">Kolor HEX</param>
         /// <returns>Kolor</returns>
-        public static Color ValueOf(string hex)
+        public static Color ColorOf(string hex)
         {
             int offset = (hex[0] == '#') ? 1 : 0;
 
@@ -27,6 +27,55 @@ namespace Omlcs.src.graphics
             }
             else
                 return Color.FromArgb(r, g, b);
+        }
+        
+        public static Color ColorOf (int hueDeg, float saturation, float lightness)
+        {
+            int d = (int)(saturation * (1 - Math.Abs(2 * lightness - 1)));
+            int m = (int)(lightness - d / 2);
+            int x = (int)(d * (1 - Math.Abs((hueDeg / 60) % 2 - 1)) + m);
+
+            int r, g, b;
+
+            if (hueDeg < 60)
+            {
+                r = 255 * d + m;
+                g = 255 * x + m;
+                b = m;
+            }
+            else if (hueDeg < 120)
+            {
+                r = 255 * x + m;
+                g = 255 * d + m;
+                b = m;
+            }
+            else if (hueDeg < 180)
+            {
+                r = m;
+                g = 255 * d + m;
+                b = 255 * x + m;
+            }
+            else if (hueDeg < 240)
+            {
+                r = m;
+                g = 255 * x + m;
+                b = 255 * d + m;
+            }
+            else if (hueDeg < 300)
+            {
+                r = 255 * x + m;
+                g = m;
+                b = 255 * d + m;
+            }
+            else
+            {
+                r = 255 * d + m;
+                g = m;
+                b = 255 * x + m;
+            }
+            //Będzie prościej kiedyś.
+
+            return Color.FromArgb(r, g, b);
         }
     }
 }

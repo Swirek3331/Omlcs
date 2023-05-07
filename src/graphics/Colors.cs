e7630c18cf67ca29e3d0b848bda095bd763f5bc2
Swirek3331 <karolsutkowski21@gmail.com>
2023-05-07 15:00:13 +0100

    Kanał alfa

--- src/graphics/Colors.cs
+++ src/graphics/Colors.cs
@@ -13,15 +13,20 @@ namespace Omlcs.src.graphics
         /// </summary>
         /// <param name="hex">Kolor HEX</param>
         /// <returns>Kolor</returns>
-        public static Color HexColor(string hex)
+        public static Color ValueOf(string hex)
         {
             int offset = (hex[0] == '#') ? 1 : 0;
 
             byte r = NumSys.ParseHex(hex, 0 + offset, 2 + offset);
             byte g = NumSys.ParseHex(hex, 2 + offset, 4 + offset);
             byte b = NumSys.ParseHex(hex, 4 + offset, 6 + offset);
-
-            return Color.FromArgb(r, g, b);
+            if (hex.Length == 8 + offset)
+            {
+                byte a = NumSys.ParseHex(hex, 6 + offset, 8 + offset);
+                return Color.FromArgb(a, r, g, b);
+            }
+            else
+                return Color.FromArgb(r, g, b);
         }
     }
 }

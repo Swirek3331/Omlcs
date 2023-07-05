880f2f20f6db63f0ccfe715459fff816de199e2b
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-07-05 13:03:21 +0200

    JEST

--- Src/Omlcs/Util/Table.cs
+++ Src/Omlcs/Util/Table.cs
@@ -7,23 +7,21 @@ namespace Omlcs.Util
     {
         public static void Swap<T>(T[] array, int a, int b)
         {
-            int temp = array[a];
+            T temp = array[a];
             array[a] = array[b];
             array[b] = temp;
         }
 
-        public static T Next<T>(T array[], T current)
+        public static T Next<T>(T[] array, T current)
         {
             int index = Array.IndexOf(array, current);
-            switch (index)
-            {
-                case -1:
-                    throw new ArgumentException();
-                case array.Length - 1:
-                    return array[0];
-                default:
-                    return array[index + 1];
-            }
+            //Niestety switch nie chce działać z wartością zmienną.
+            if (index == -1)
+                throw new ArgumentException("Brak elementu w tablicy");
+            if (index == array.Length - 1)
+                return array[0];
+            else
+                return array[index + 1];
         }
     }
 }

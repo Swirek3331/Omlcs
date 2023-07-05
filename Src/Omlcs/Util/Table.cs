3c9b3eabcbc306a35152cf7e6d5d251e92fb1502
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-07-05 12:55:31 +0200

    Next

--- Src/Omlcs/Util/Table.cs
+++ Src/Omlcs/Util/Table.cs
@@ -11,5 +11,19 @@ namespace Omlcs.Util
             array[a] = array[b];
             array[b] = temp;
         }
+
+        public static T Next<T>(T array[], T current)
+        {
+            int index = Array.IndexOf(array, current);
+            switch (index)
+            {
+                case -1:
+                    throw new ArgumentException();
+                case array.Length - 1:
+                    return array[0];
+                default:
+                    return array[index + 1];
+            }
+        }
     }
 }

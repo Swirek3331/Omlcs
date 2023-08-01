0ce5ff90d190578bf6ff1789948a11e1fa1fe51d
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-08-01 18:33:06 +0200

    E no, to LINUX GAMING

--- Drawing/Hue.cs
+++ Drawing/Hue.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
 using static Omlcs.Util.Base;
 
-namespace Omlcs.Graphic
+namespace Omlcs.Drawing
 {
     public static class Hue
     {

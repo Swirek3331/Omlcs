51840c3ae5375db2641593069bd4e6f0e04dd63e
Swirek3331 <karolsutkowski21@gmail.com>
2023-09-17 18:18:00 +0100

    I jest

--- IO/Termianl.cs
+++ IO/Termianl.cs
@@ -1,8 +1,17 @@
 using System;
 
-namespace Omlcs.IO;
-
-public static class Terminal
+namespace Omlcs.IO
 {
-    
+    public static class Terminal
+    {
+        public static void Printf(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void Printf(object obj)
+        {
+            Console.WriteLine(obj.ToString());
+        }
+    }
 }
---
+++ Structures/Arrays.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static Omlcs.IO.Terminal;
+
+namespace Omlcs.Structures
+{
+    public static class Arrays
+    {
+        public static void Show<T>(T[] array)
+        {
+            foreach (T item in array)
+            {
+                Printf(item);
+            }
+        }
+
+        public static void Show<T>(T[] array, int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                Printf(array[i]);
+            }
+        }
+
+        public static void Swap<T>(T[] array)
+        {
+            T temp = array[0];
+            array[0] = array[1];
+            array[1] = temp;
+        }
+    }
+}

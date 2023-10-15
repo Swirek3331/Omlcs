87a01cffd4a55f4d6c83fb02bc5517c35aefb265
Swirek3331 <karolsutkowski21@gmail.com>
2023-10-15 12:27:53 +0100

    Update Arrays.cs

--- Collections/Arrays.cs
+++ Collections/Arrays.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO.Pipes;
 using System.Security.Cryptography.X509Certificates;
 using Omlcs.MathEtc;
 
@@ -11,25 +12,25 @@ namespace Omlcs.Collections
         public static T Next<T>(T[] array, T current)
         {
             int index = Array.IndexOf(array, current);
-
+            int lenght = array.Length - 1;
             if (index == -1)
-            {
                 throw new ArgumentException("The current value is not in the array.");
-            }
             else if (index == array.Length - 1)
-            {
                 return array[0];
-            }
-            else
-            {
-                return array[index + 1];
-            }
-
+            return array[index + 1];
         }
 
         public static void FillAnArray<T>(T[] array, T value)
         {
             for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+        }
+
+        public static void FillAnArray<T>(T[] array, T value, int start, int end)
+        {
+            for (int i = start; i < end; i++)
             {
                 array[i] = value;
             }
@@ -50,6 +51,30 @@ namespace Omlcs.Collections
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = Randomite.Next(min, max);
+            }
+        }
+
+        public static void FillAnArray(int[] array, int min, int max, int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                array[i] = Randomite.Next(min, max);
+            }
+        }
+
+        public static void Swap<T>(T[] array, int aIndex, int bIndex)
+        {
+            T buffer = array[aIndex];
+            array[aIndex] = array[bIndex];
+            array[bIndex] = buffer;
+
+        }
+
+        public static void Reverse<T>(T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Swap(array, i, array.Length - 1 - i);
             }
         }
     }

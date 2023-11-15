4e71c096df21448d92c4a72811999c661012ba58
Swirek3331 <karolsutkowski21@gmail.com>
2023-11-15 18:42:30 +0100

    Update FormsDialog.cs

--- IO/FormsDialog.cs
+++ IO/FormsDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 using static System.Windows.Forms.MessageBox;
@@ -7,6 +8,20 @@ namespace Omlcs.IO
 {
     public static class FormsDialog
     {
+        internal static Form inputWindow = new()
+        {
+            //TODO: size, position, etc.
+            ClientSize = new Size(50, 50),
+            Text = "Prompt",
+            Controls =
+            {
+                new Label(),
+                new TextBox(),
+                new Button(),
+                new Button(),
+            }
+        };
+
         public static void Message(string name, string description)
         {
             Show(description, name, MessageBoxButtons.OK);
@@ -20,6 +35,14 @@ namespace Omlcs.IO
                 return true;
             }
             return false;
+        }
+
+        //TODO: implement
+        private static string Prompt(string name, string label, string defaultText)
+        {
+            inputWindow.Text = name;
+            inputWindow.Controls[0].Text = label;
+            inputWindow.Controls[1].Text = defaultText;
         }
     }
 }

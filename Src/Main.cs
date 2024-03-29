8f7afe3dc40453a424ab150a0d64632d538566a8
Karol Sutkowski <karolsutkowski21@gmail.com>
2024-03-29 15:32:53 +0100

    Zaokrąglanie?

--- Src/Main.cs
+++ Src/Main.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Omlcs.Src
+namespace Omlcs
 {
     public class Main
     {
+        public static int Floor(float x)
+        {
+            var n = x.ToString().Split('.');
 
+            return int.Parse(n[0]);
+        }
+
+        public static int Round(float x)
+        {
+            int n;
+
+            if (x % 1 >= 0.5)
+            {
+                n = Ceil(x);
+            }
+            else
+            {
+                n = Floor(x);
+            }
+
+            return n;
+        }
+
+        public static int Ceil(float x)
+        {
+            return Floor(x) + 1;
+        }
     }
 }
